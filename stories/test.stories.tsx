c1be98d94333a397359891154c969b0134c4b5e8
Andrew Zures <andrew.zures@gmail.com>
2018-04-23 20:10:48 -0500

    working out imports

---
+++ stories/test.stories.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+import { storiesOf } from '@storybook/react';
+
+storiesOf('Another Button', module)
+    .add('something', () => (
+        <button>Hi</button>
+    ));
